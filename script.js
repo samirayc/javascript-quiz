a7706e9d2b27b19b475631e09c0dc3e507f05aaf
Samira Chetta <chettasamira@gmail.com>
2023-11-04 16:59:20 -0400

    finished html and added placeholder styling, beginning to add questions to js

---
+++ script.js
@@ -0,0 +1,11 @@
+var questions = [
+    {
+        question: 'What is the correct syntax for referring to an external script called "abc.js"?',
+        choices: ['script src="abc.js"', 'script href="abc.js"', 'script name="abc.js"'],
+        correct: 0
+    },
+    {
+        question: 'Which operator is used to assign a value to a variable?',
+        choices: []
+    }
+]
