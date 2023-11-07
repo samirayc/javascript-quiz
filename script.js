ce2e1ac7a97f06a1d7f916244df35df0bad5d37d
Samira Chetta <chettasamira@gmail.com>
2023-11-06 20:43:35 -0500

    bug fixes, score now shows up

--- script.js
+++ script.js
@@ -6,6 +6,56 @@ var questions = [
     },
     {
         question: 'Which operator is used to assign a value to a variable?',
-        choices: []
+        choices: ['-', '=', 'X'],
+        correct: 1
+    },
+    {
+        question: 'Which event occurs when the user clicks on an HTML element?',
+        choices: ['onmouseclick', 'onclick', 'onmouseover'],
+        correct: 1
+    },
+    {
+        question: 'Which of the following will write the message "Hello World!" in an alert box?',
+        choices: ['alertBox("Hello World!");', 'alert(Hello World!);', 'alert("Hello World!");'],
+        correct: 2
+    },
+];
+
+let currentQuestion = 0;
+let correctAnswers = 0;
+
+function showQuestion() {
+    const questionText = document.getElementById("question-text");
+    questionText.textContent = questions[currentQuestion].question;
+  
+    const choices = document.querySelectorAll(".choice");
+    choices.forEach((choice, index) => {
+      choice.textContent = questions[currentQuestion].choices[index];
+    });
+    const response = document.getElementById("response");
+    response.textContent = "";
+  }
+  
+  function checkAnswer(selected) {
+    const response = document.getElementById("response");
+    if (selected === questions[currentQuestion].correct) {
+      response.textContent = "Correct!";
+      correctAnswers++;
+    } else {
+      response.textContent = "Incorrect!";
     }
-]
+  
+    setTimeout(() => {
+      currentQuestion++;
+  
+      if (currentQuestion < questions.length) {
+        showQuestion();
+      } else {
+        const quizContainer = document.querySelector(".quiz");
+        quizContainer.innerHTML = `<p>You got ${correctAnswers} out of ${questions.length} questions.</p>`;
+      }
+    }, 1000);
+  }
+  
+  showQuestion();
+  
